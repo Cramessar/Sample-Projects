4423c3714600daa9895565d8a57c6547dd4fd527
Cramessar <154475437+Cramessar@users.noreply.github.com>
2024-10-28 12:50:37 -0400

    Update api_keys.py

--- octoberprojects/CrewAI/api_keys.py
+++ octoberprojects/CrewAI/api_keys.py
@@ -1,3 +1,5 @@
+###Good idea to have a master page for any and all api keys. 
+###Try to created uqiue function names for each api, openai, huggingface, etc
 def get_openai_api_key():
     # Your function logic here
-    return "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"
+    return "Your OPENAI API Key Here"
