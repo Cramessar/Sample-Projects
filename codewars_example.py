9696ddff63f04f5c1eebc77c64ec783ffc86a2f9
Cramessar <154475437+Cramessar@users.noreply.github.com>
2024-12-01 18:32:31 -0500

    moved files

--- codewars_example.py
+++
@@ -1,8 +0,0 @@
-def sum_array(arr):
-    if arr is None or len(arr) <= 2:
-        return 0
-    else: 
-        arr.remove(max(arr))
-        arr.remove(min(arr))
-        r = sum(arr)
-        return r
